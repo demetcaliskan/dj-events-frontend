76ee1526b83d71dfda3815d6f2e6f1ea195ef801
demetcaliskan <demetcaliskann@gmail.com>
2022-03-12 02:27:58 +0300

    images and showcase component is added

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,12 @@
 import Head from 'next/head'
+import { useRouter } from 'next/router'
 import Header from './Header'
 import Footer from './Footer'
+import Showcase from './Showcase'
 import styles from '@/styles/Layout.module.css'
 
 export default function Layout({ title, description, children }) {
+    const router = useRouter()
     return (
         <div>
             <Head>
@@ -11,6 +14,7 @@ export default function Layout({ title, description, children }) {
                 <meta name='description' content={description} />
             </Head>
             <Header />
+            {router.pathname === '/' && <Showcase />}
             <div className={styles.container}>
                 {children}
             </div>
